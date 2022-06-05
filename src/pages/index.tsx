559b3cc2c3c3f243abf766b7e991f78e37caad46
Leonardo Lemos <leonardo.lemos2002@hotmail.com>
2022-06-05 10:38:00 -0300

    ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
+
+//Client-side
+//Server-side
+//Statis Site Generation
 
 interface HomeProps {
   product: {
@@ -39,7 +43,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1L6bQtAV06oaKw2iOCZrl5zJ');
 
   const product = {
@@ -53,6 +57,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
